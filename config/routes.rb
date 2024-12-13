5f5b7704271d00c748c71b7fe78d13d32a1a4be7
tsutsumi1105 <backnumber_flower@icloud.com>
2024-12-14 01:55:40 +0900

    ログイン、ログアウト機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
   # root "posts#index"
   root 'tasks#index'
   resources :users, only: %i[new create]
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
