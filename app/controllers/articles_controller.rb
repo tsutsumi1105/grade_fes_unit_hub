f5f9a8cdf4f4cea33935e675a79acfe8ee3be957
tsutsumi1105 <backnumber_flower@icloud.com>
2024-12-14 16:24:48 +0900

    記事の投稿機能実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,24 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.includes(:user)
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = current_user.articles.build(article_params)
+    if @article.save
+      redirect_to articles_path, success: "記事を投稿しました。"
+    else
+      flash.now[:danger] = "記事の投稿に失敗しました。"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
