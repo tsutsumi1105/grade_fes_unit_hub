98acc2e8c6933c40e3482c5cc8fa12ef8f8d33ad
tsutsumi1105 <backnumber_flower@icloud.com>
2025-01-08 14:29:54 +0900

    いいね機能実装

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,10 @@ class Article < ApplicationRecord
   validates :body, presence: true, length: { maximum: 65_535 }
 
   has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   belongs_to :user
+
+  def favorited_by?(user)
+    favorites.exists?(user: user)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_04_014311) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_08_033446) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_04_014311) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_favorites_on_article_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -52,4 +61,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_04_014311) do
   add_foreign_key "articles", "users"
   add_foreign_key "comments", "articles"
   add_foreign_key "comments", "users"
+  add_foreign_key "favorites", "articles"
+  add_foreign_key "favorites", "users"
 end
