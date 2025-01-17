c2728a354ba5d37b4567e1271d55c9177c54c6ba
tsutsumi1105 <backnumber_flower@icloud.com>
2025-01-17 16:25:55 +0900

    マイページ機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :require_login, only: %i[new create]
+  before_action :set_user, only: %i[show edit update mypage]
 
   def new
     @user = User.new
@@ -15,9 +16,32 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to mypage_path, success: 'ユーザー情報を更新しました'
+    else
+      render :edit, danger: '更新できませんでした'
+    end
+  end
+
+  def mypage
+    @user = current_user
+    @articles = @user.articles.order(created_at: :desc)
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = current_user
   end
 end
