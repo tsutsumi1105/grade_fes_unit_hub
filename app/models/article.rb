3e616844bfc910269091099ee29c17fa70487568
tsutsumi1105 <backnumber_flower@icloud.com>
2025-02-14 16:57:37 +0900

    Action Text導入

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   has_one_attached :thumbnail
+  has_rich_text :body
   
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65_535 }
