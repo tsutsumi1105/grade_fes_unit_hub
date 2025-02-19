31dfb6c19c2a7fc14efec97c0f2ef170f66b0567
tsutsumi1105 <backnumber_flower@icloud.com>
2025-02-20 06:22:41 +0900

    オートコンプリート検索機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,14 @@ Rails.application.routes.draw do
   # root "posts#index"
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   root 'articles#index'
-  resources :users, only: %i[new create show edit update]
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
-  resources :tags, only: %i[show]
   resources :bookmarks, only: %i[create destroy]
   get 'mypage', to: 'users#mypage', as: 'mypage'
   resources :password_resets, only: %i[new create edit update]
+  resources :users, only: %i[new create show edit update]
+  resources :tags, only: %i[show]
 
   resources :articles, only: %i[index new create show edit update destroy] do
     resources :comments, only: %i[create edit update destroy]
@@ -24,8 +24,11 @@ Rails.application.routes.draw do
     get :tags, on: :collection
     collection do
       get :bookmarks
+      get :autocomplete_title
+      get :autocomplete_user_name
+      get :autocomplete_tags_name
     end
   end
 
-  #match '*unmatched_route', to: 'errors#routing_error', via: :all, constraints: lambda { |req| !req.path.start_with?('/rails/active_storage/') }
+  match '*unmatched_route', to: 'errors#routing_error', via: :all, constraints: lambda { |req| !req.path.start_with?('/rails/active_storage/') }
 end
