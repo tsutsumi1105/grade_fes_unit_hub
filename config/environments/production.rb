9f676f6fb35e4982ea2adf6787e4e46e71cc014b
tsutsumi1105 <backnumber_flower@icloud.com>
2025-02-24 09:46:33 +0900

    送信用メールアドレスの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,4 +96,15 @@ Rails.application.configure do
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.action_mailer.default_url_options = { host: 'gradefesunithub.com' }
   config.hosts << 'gradefesunithub.com'
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => '587',
+    :domain => 'smtp.gmail.com',
+    :authentication => 'plain',
+    :user_name => ENV['SMTP_USER_NAME'],
+    :password => ENV['SMTP_PASSWORD']
+  }
 end
