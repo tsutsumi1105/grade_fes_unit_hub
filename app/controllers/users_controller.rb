81abe8942be398aff72502afc837cea50641c0b5
tsutsumi1105 <backnumber_flower@icloud.com>
2025-02-27 10:02:57 +0900

    users_showの調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @articles = @user.articles.order(created_at: :desc)
+    @articles = @user.articles.where.not(status: :draft).order(created_at: :desc)
   end
 
   def edit
